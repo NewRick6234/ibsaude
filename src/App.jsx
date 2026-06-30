e29c22cfae277c8558fda7edebe05d8ee717c241
Cauã Ferreto <cauaferreto3@gmail.com>
2026-06-30 17:39:40 -0300

    Cadastro compilado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import Cadastro from '../cadastro'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <> 
+
+     <Link to='/cadastro'>Cadastrar</Link>
+
+
+    <Routes>
+      <Route path='cadastro' element={<Cadastro/>}> Cadastrar</Route>
+    </Routes>
+
+
+   
     </>
   )
 }
