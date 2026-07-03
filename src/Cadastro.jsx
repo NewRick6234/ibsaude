5fa1d6045bf9166dd43b1cd61f0b1d7151232ce4
Onurb <hellangel093@gmail.com>
2026-07-02 22:18:51 -0300

    alterações na pagina de cadastro

--- src/Cadastro.jsx
+++ src/Cadastro.jsx
@@ -1,21 +1,26 @@
-import Input from '../Input'
+import Input from './componentes/Input'
 import Botao from './Botao';
 
 function Cadastro(){
 
     return(
-    <form className='flex flex-col items-center' method='post'>
-     <Input name="Nome do medicamento"></Input>
-     <Input name="Classificação" ></Input>
-     <Input name="Codigo interno e/ ou código oficial" ></Input>
-     <Input name="Uso específico" ></Input>
-     <Input name="Tipo" ></Input>
-     <Input name="Fabricante" ></Input>
-     <Input name="Lote" ></Input>
-     <Input name="Data de validade" tipo='date' ></Input>
+        <>
+    <div className='flex flex-col items-center h-full'>
+    <h1 className='text-3xl font-semibold tracking-tight text--900 sm:text-3xl '>Cadastro de Medicamentos</h1>
+    <form className='flex flex-col items-center h-screen' method='post'>
+     <Input name="Nome do medicamento:"></Input>
+     <Input name="Classificação:" ></Input>
+     <Input name="Codigo interno e/ ou código oficial:" ></Input>
+     <Input name="Uso específico:" ></Input>
+     <Input name="Tipo:" ></Input>
+     <Input name="Fabricante:" ></Input>
+     <Input name="Lote:" ></Input>
+     <Input name="Data de validade:" tipo='date' ></Input>
     
     <Botao></Botao>
     </form>
+    </div>
+    </>
     )
 
 }
