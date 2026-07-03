e4874d98c7504246e35f07edaa1041298fc05b66
Onurb <hellangel093@gmail.com>
2026-07-02 21:17:14 -0300

    Atualização da tela de cadastro

--- src/Cadastro.jsx
+++ src/Cadastro.jsx
@@ -5,8 +5,14 @@ function Cadastro(){
 
     return(
     <form className='flex flex-col items-center' method='post'>
-     <Input name="Usuario"></Input>
-     <Input name="Email" tipo="email"></Input>
+     <Input name="Nome do medicamento"></Input>
+     <Input name="Classificação" ></Input>
+     <Input name="Codigo interno e/ ou código oficial" ></Input>
+     <Input name="Uso específico" ></Input>
+     <Input name="Tipo" ></Input>
+     <Input name="Fabricante" ></Input>
+     <Input name="Lote" ></Input>
+     <Input name="Data de validade" tipo='date' ></Input>
     
     <Botao></Botao>
     </form>
