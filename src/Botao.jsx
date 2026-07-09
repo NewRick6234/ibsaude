2f905e8189e5a90dd478a9fd976798e8af37db09
Bruno <hellangel093@gmail.com>
2026-07-09 18:41:56 -0300

    Criação da tabela com informaçoes de nfe

--- src/Botao.jsx
+++ src/Botao.jsx
@@ -1,8 +1,8 @@
-function Botao({children}){
+function Botao({children,...props}){
 
     return(
-     
-        <button type="button" className="flex w-full m-8 justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enviar</button>
+        
+        <button {...props} type="button" className="flex w-full m-8 justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enviar</button>
       )
 
     }
