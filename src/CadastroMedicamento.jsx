93430ed77718add5877da1373ddde4da18330253
Bruno <hellangel093@gmail.com>
2026-08-11 18:51:09 -0300

    alteracao no estilo

--- src/CadastroMedicamento.jsx
+++ src/CadastroMedicamento.jsx
@@ -8,7 +8,7 @@ function CadastroMedicamento(){
         <>
     <div className='flex flex-col gap-5 items-center h-full'className="w-full" border="1" cellPadding="8" cellSpacing="0"> 
 
-    <h1 className='text-3xlfont-bold tracking-tight text-gray-100 sm:text-3xl '>Cadastro de Medicamentos</h1>
+    <h1 className='text-3xlfont-bold tracking-tight text-black-100 sm:text-3xl '>Cadastro de Medicamentos</h1>
     <form className='flex flex-col gap-4 items-center h-full' method='post'>
         <Input name="Nome do medicamento:">Nome do Medicamento</Input>
        <Input name="Classificação:" options={['Venda Livre', 'Antibiótico', 'Controlado (Receita Especial)', 'Uso Hospitalar']}>Classificação</Input>
