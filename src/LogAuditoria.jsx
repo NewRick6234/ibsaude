cf2aa367e0e7ec5f05ce25372954a14c3cc989b6
Onurb <hellangel093@gmail.com>
2026-08-30 22:53:12 -0300

    alinhamento corrigido no log auditoria

--- src/LogAuditoria.jsx
+++ src/LogAuditoria.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom'
 import Botao from './componentes/Botao'
 import Input from './componentes/Input'
-import { Weight } from 'lucide-react'
+import { Download, FileText, FileSpreadsheet } from 'lucide-react'
 function LogAuditoria(){
 
    
     return(
-        <><div className='min-w-5xl w-full flex flex-col gap-4 items-center justify-center'>
+        <><div className='w-full mx-auto px-4 sm:px-6 lg:px-8 py-6 flex flex-col gap-6'>
             <div className='w-full grid grid-cols-1 md:grid-cols-2 mb-8 items-center justify-center'>
                             <div className="bg-surface-container-lowest p-4 rounded-lg">
                                             <p className="p-10 uppercase">Logs de Auditoria</p>
@@ -14,20 +14,22 @@ function LogAuditoria(){
                                     
                             </div>
                              
-                            <div className="w-full bg-surface-container-lowest justify-center items-center flex gap-5 p-4 rounded-lg max-w-100">
-                                <Botao className="mb-1 mt-1 w-75">
-                                    Escanear Lote
+                            <div className="w-full bg-surface-container-lowest justify-center items-center flex gap-5 p-4 rounded-lg">
+                                <Botao className="w-full">
+                                    <Download className="flex w-4" />
+                                    Export CSV
                                 </Botao>
-                                <Botao className="mb-1 mt-1 w-75">
-                                    Nova entrada
+                                <Botao className="w-full">
+                                    <FileText className="flex w-4 " />
+                                    Export CSV
                                 </Botao>
                                      
                             </div>
                              
             </div>
 
-            <div className='min-w-5xl w-full flex flex-col gap-4 items-center justify-center'>
-                <div className='w-full grid grid-cols-1 md:grid-cols-4 gap-4 mb-8'>
+            <div className='w-full flex flex-col gap-4 items-center justify-center'>
+                <div className='w-full border rounded-lg grid grid-cols-1 md:grid-cols-4 gap-4 mb-8'>
                     <Input
                         type="date"
                     >
